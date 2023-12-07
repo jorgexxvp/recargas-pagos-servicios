a3a8fe6e1fea27ede921ef1b629baae04be1ba9f
jorgexVP <89286580+jorgexVP@users.noreply.github.com>
2023-12-06 20:21:28 -0500

    Fix(Enpoint)

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: "http://bun-burn-env.eba-ftyx2m3h.us-east-1.elasticbeanstalk.com",
+  baseURL: "https://bun-burn-env.eba-ftyx2m3h.us-east-1.elasticbeanstalk.com",
   headers: {
      Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Indpbmdlcmxpb24uY3VscWkiLCJleHAiOjE3MDY5NDIxNjR9.j7_UICJFBvPiHv_bhnoRNprVU_DaML41Vjmd51jcRDI"
   }
